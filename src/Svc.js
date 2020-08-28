14c184de8d171f7330858e78eeee13558b24cf20
Matt Conway <matt@conwaysplace.com>
2020-08-28 10:18:13 -0400

    add service fetch

---
+++ src/Svc.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import ReactJson from 'react-json-view'
+
+function Svc(props) {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [data, setData] = useState({});
+
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+        fetch(props.address)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    console.log(result);
+                    setData(result);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [props.address])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return <ReactJson theme="colors" name={false} collapsed={false} src={data} />
+    }
+}
+
+export default Svc;
