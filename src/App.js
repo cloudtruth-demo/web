6798c7ce60446088fd02035a3f93fbb484e191a7
Todd OBrien <59969475+obrietodd@users.noreply.github.com>
2020-10-13 13:47:09 -0400

    Update App.js
    added CTChart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from './ctlogo.png';
 import './App.css';
 import Svc from './Svc';
+import CTChart from './CTChart';
 import useInterval from 'use-interval';
 
 function App() {
@@ -27,13 +28,13 @@ function App() {
                     href="https://github.com/cloudtruth-demo/ops">ops repository</a></li>
                 </ul>
                 <h4>Dynamic configuration within the Web Application</h4>
-                <p> The following JSON block is fetched directly by the web application from a CloudTruth configuration template</p>
+                <p> The following chart is configured via JSON fetched directly by the web application from a CloudTruth configuration template</p>
                 <ul>
                     <li>This demonstrates the use of a CloudTruth template as a dynamic source of configuration for a web application at runtime</li>
                     <li>Changes to the template or the parameters it references will be picked up immediately and shown below:</li>
                 </ul>
                 <div className="SVC-block">
-                    <Svc address={"https://api.cloudtruth.com/t/" + process.env.REACT_APP_CONFIG_TID + "/" + process.env.REACT_APP_ENV} count={count}/>
+                    <CTChart address={"https://api.cloudtruth.com/t/" + process.env.REACT_APP_CONFIG_TID + "/" + process.env.REACT_APP_ENV} count={count}/>
                 </div>
                 <h4>Static and Dynamic configuration within a Service</h4>
                 <p>The following JSON block is fetched from a simple JSON service ({process.env.REACT_APP_DEMO1_ADDRESS})</p>
@@ -41,8 +42,8 @@ function App() {
                     <li>This JSON block represents the current configuration for the service</li>
                     <li>This configuration consists of a static portion supplied at deploy time, 
                     and live portion fetched from a CloudTruth template</li>
-                    <li>Changes to the static configuration will take affect the next time the service is deployed</li>
-                    <li>Changes to the live configuration will take affect immediately</li>
+                    <li>Changes to the static configuration will take effect the next time the service is deployed</li>
+                    <li>Changes to the live configuration will take effect immediately</li>
                 </ul>
                 <div className="SVC-block">
                     <Svc address={process.env.REACT_APP_DEMO1_ADDRESS} count={count}/>
