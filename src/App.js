6c8736e72af053ee38db88ca3d69474def5b523a
Todd OBrien <59969475+obrietodd@users.noreply.github.com>
2021-01-20 12:38:43 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -41,18 +41,18 @@ function App() {
                  </div>
                  <div class="summary-block">
                      <div class="summay-text">
-                         <p>AMCE uses Terraform to provision their cloud environment, AWS SSM Param Store for secrets and JSON files
-                            in S3 for managing application configuration.</p>
+                         <p>ACME relies upon Terraform to provision their cloud environment,
+                            AWS SSM Parameter Store for secrets and JSON files in S3 buckets for application configuration.</p>
                      </div>
                  </div>
                  <div class="use-case-block">
                      <div class="use-case-text">
                          <h3>ACME uses CloudTruth Configuration Intelligence to solve the following problems:</h3>
                          <ol>
-                             <li><a href="#webapp">Dynamic configuration with a web application</a></li>
-                             <li><a href="#websvc">Static and dynamic configuration of a service</a></li>
-                             <li><a href="#gacicd">Deployment to AWS using Github Actions for CI/CD</a></li>
-                             <li><a href="#kubapp">Automatic updates for Kubernetes ConfigMaps</a></li>
+                             <li class="use-case-list-text"><a href="#webapp">Dynamic configuration with a web application</a></li>
+                             <li class="use-case-list-text"><a href="#websvc">Static and dynamic configuration of a service</a></li>
+                             <li class="use-case-list-text"><a href="#gacicd">Cloud deployment using standard CI/CD tools</a></li>
+                             <li class="use-case-list-text"><a href="#kubapp">Management of applications & service configuration for Kubernetes</a></li>
                          </ol>
                      </div>
                  </div>
@@ -66,9 +66,9 @@ function App() {
                                  from a CloudTruth configuration template</h4>
                              <ul>
                                  <li>This demonstrates the use of a CloudTruth template as a dynamic source of configuration
-                                     for a web application at runtime</li>
+                                     for a web application at runtime.</li>
                                  <li>Changes to the template or the parameters it references will be picked up immediately
-                                     and shown below:</li>
+                                     and shown below.</li>
                              </ul>
                          </div>
                      </div>
@@ -80,8 +80,8 @@ function App() {
                      </div>
                      <div class="app-info-bottom">
                          <ul>
-                             <li>The CloudTruth parameter management screen is shown on the bottom left</li>
-                             <li>The CloudTruth template results in JSON format are shown on the botton right</li>
+                             <li>The CloudTruth parameter management screen is shown on the bottom left.</li>
+                             <li>The CloudTruth template results in JSON format are shown on the botton right.</li>
                          </ul>
                      </div>
                      <div class="app-block-bottom">
@@ -114,12 +114,12 @@ function App() {
                              <h4>The following JSON block is fetched from a simple JSON service
                                  (<a href="https://github.com/cloudtruth-demo">https://demo1.demo.cloudtruth.dev</a>)</h4>
                              <ul>
-                                 <li>This JSON block represents the current configuration for the service</li>
+                                 <li>This JSON block represents the current configuration for the service.</li>
                                  <li>This configuration consists of a static portion supplied at deploy time, 
-                                     and live portion fetched from a CloudTruth template</li>
+                                     and live portion fetched from a CloudTruth template.</li>
                                  <li>Changes to the static configuration will take effect the next time the
-                                     service is deployed</li>
-                                 <li>Changes to the live configuration will take effect immediately</li>
+                                     service is deployed.</li>
+                                 <li>Changes to the live configuration will take effect immediately.</li>
                              </ul>
                          </div>
                      </div> 
@@ -143,17 +143,17 @@ function App() {
                      </div>
                      <div class="cicd-block">
                          <div class="cicd-text" id="gacicd">
-                             <h2>Deployment to AWS using <a href="https://github.com/cloudtruth-demo">Github Actions for CI/CD</a></h2>
+                             <h2>Cloud deployment using standard CI/CD tools</h2>
                              <ul>
                                  <li>This application interacts with a simple JSON service in order to demonstrate 
-                                     a complete system configured by CloudTruth</li>
+                                     a complete system configured by CloudTruth.</li>
                                  <li>Both the <a href="https://github.com/cloudtruth-demo/web">web client</a> and
                                      the <a href="https://github.com/cloudtruth-demo/service">json
-                                     service</a> are deployed to AWS using Github Actions for CI/CD</li>
+                                     service</a> are deployed to AWS using Github Actions for CI/CD.</li>
                                  <li>You can browse (.github/workflows/cd.yml) within each repository as a guide for 
-                                     integrating CloudTruth with your own CI/CD system</li>
+                                     integrating CloudTruth with your own CI/CD system.</li>
                                  <li>The system is provisioned in AWS using terraform with the <a
-                                     href="https://github.com/cloudtruth-demo/ops">ops repository</a></li>
+                                     href="https://github.com/cloudtruth-demo/ops">ops repository.</a></li>
                              </ul>
                          </div>    
                      </div>
@@ -182,14 +182,14 @@ function App() {
                      </div>
                      <div class="kube-block">
                          <div class="kube-text" id="kubapp">
-                             <h2>Automatic updates for Kubernetes ConfigMaps</h2>
+                             <h2>Management of applications & service configuration for Kubernetes</h2>
                              <ul>
                                  <li>The CloudTruth integration for Kubernetes pushes parameter updates into ConfigMaps and Secrets.</li>
-                                 <li>This integration uses naming conventions to automate the delivery of configuration</li>
+                                 <li>This integration uses naming conventions to automate the delivery of configuration.</li>
                                  <li>You can browse (https://github.com/cloudtruth/kubetruth) as a guide for 
-                                     integrating CloudTruth with your Kubernetes cluster</li>
+                                     integrating CloudTruth with your Kubernetes cluster.</li>
                                  <li>The KubeTruth code is hosted in the <a
-                                     href="https://github.com/cloudtruth/kubetruth">KubeTruth repository</a></li>
+                                     href="https://github.com/cloudtruth/kubetruth">KubeTruth repository.</a></li>
                              </ul>
                          </div>
                      </div>
